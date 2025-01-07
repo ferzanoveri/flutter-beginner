7032e31946e8a9e00a21b99ab1675a6ddc0462f9
ferzanoveri <ferzanoveri@gmail.com>
2025-01-07 23:46:06 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Hello'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('Hello Ferza!'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Text('Click'),
-    ),
-  ),
+  home: Home(),
 ));
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: ElevatedButton.icon(
+            onPressed: (){
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Awesome Snackbar'),
+                ),
+              );
+            },
+          icon: Icon(Icons.mail),
+          label: Text('mail me'),
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.amberAccent),
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text(
+          'Click',
+          style: TextStyle(
+              color: Colors.white
+          ),
+        ),
+        backgroundColor: Colors.red[600],
+
+      ),
+    ) ;
+  }
+}
+
